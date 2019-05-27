d089b4d4320608747692ef550e3ac47e426b8d64
Simon Bundgaard-Egeberg <seg@it-minds.dk>
2019-05-27 09:53:06 +0200

    More dots

--- src/routes/_utils/stores.js
+++ src/routes/_utils/stores.js
@@ -7,7 +7,13 @@ const motivationalQuotes = [
     "Du er en true champion",
     "Der findes ingen der er lige så sej som dig!",
     "Du er igang med at ændre verden!",
-    "I'm the best, fuck the rest!"
+    "I'm the best, fuck the rest!",
+    "Du er en sand gud blandt mennesker",
+    "Hvis du var en karamel sundae, så var det en med dobbelt karamel",
+    "Du er bedre en Eskild's burger torsdag",
+    "Hvis en tomat var en frugt, så er du stadig mega nice!",
+    "Salat til frokost?",
+    "Hvis det er tirsdag, så er der måske kage!??!!?!",
 ]
 
 
